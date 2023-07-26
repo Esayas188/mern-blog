86e661fb60085f17511282724ad96aa172ba7d5c
Esayas188 <esayasfekade188@gmail.com>
2023-07-26 07:15:46 -0400

    finished

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,37 +1,36 @@
-import { useEffect } from "react"
+import { useEffect }from 'react'
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
 
 // components
-import WorkoutDetails from "../components/WorkoutDetails"
-import WorkoutForm from "../components/WorkoutForm"
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
-  const { workouts, dispatch } = useWorkoutsContext()
+  const {workouts, dispatch} = useWorkoutsContext()
+  const {user} = useAuthContext()
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts')
+      const response = await fetch('/api/workouts', {
+        headers: {'Authorization': `Bearer ${user.token}`},
+      })
       const json = await response.json()
 
       if (response.ok) {
         dispatch({type: 'SET_WORKOUTS', payload: json})
       }
     }
-    // Define the interval for long polling in milliseconds
-    const interval = 5000 // Fetch updates every 5 seconds
-
-    // Create a timer to repeatedly fetch the latest workouts
-    const timerId = setInterval(fetchWorkouts, interval)
-
-    // Clean up the timer when the component unmounts
-    return () => clearInterval(timerId)
-  }, [dispatch])
+    if (user) {
+      fetchWorkouts()
+    }
+  }, [dispatch, user])
 
   return (
     <div className="home">
       <div className="workouts">
-        {workouts && workouts.map(workout => (
-          <WorkoutDetails workout={workout} key={workout._id} />
+        {workouts && workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout} />
         ))}
       </div>
       <WorkoutForm />
